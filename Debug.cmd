d313de904a15c81f464fc97c6cea24ddc7b7c3d6
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2021-12-04 19:11:24 +0100

    Add files via upload

--- Debug.cmd
+++ Debug.cmd
@@ -1,2 +1,11 @@
-call Compile.cmd
-qemu-system-x86_64 Vortex.bin
+nasm "bootloader.asm" -f bin -o "Binaries/boot.bin"
+nasm "kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
+gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "Binaries/kernel.o"
+nasm "zeros.asm" -f bin -o "Binaries/zeroes.bin"
+
+C:\i386\i386-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
+
+copy "Binaries/boot.bin"+"Binaries/full_kernel.bin" "Binaries/zeroes.bin" "Binaries/OS.bin"
+
+qemu-system-x86_64 -drive format=raw,file="Binaries/OS.bin",index=0,if=floppy,  -m 128M
+pause
--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,4 +1,4 @@
 [bits 32]
-[extern main]
-call main
+[extern ___main]
+call ___main
 jmp $
