dbd952f70948b3b336a062f17e50c52b7ad02c75
David Gerson <84614495+pogrammerX@users.noreply.github.com>
2021-12-04 20:06:32 +0100

    Added ld Tools

--- Debug.cmd
+++ Debug.cmd
@@ -3,7 +3,7 @@ nasm "kernel_entry.asm" -f elf -o "Binaries/kernel_entry.o"
 gcc -ffreestanding -m32 -g -c "kernel.cpp" -o "Binaries/kernel.o"
 nasm "zeros.asm" -f bin -o "Binaries/zeroes.bin"
 
-C:\i386\i386-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
+i386-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
 
 copy "Binaries/boot.bin"+"Binaries/full_kernel.bin"+"Binaries/zeroes.bin" "Binaries/OS.bin"
 
